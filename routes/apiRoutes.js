07d8436c0471692939721395a08faa5c23eee475
Alejandro Schmidt <alemusho@gmail.com>
2021-05-19 12:43:55 -0300

    ok

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,7 +12,7 @@ router.post("/tokens", loginController.login);
 // CRUD de usuarios
 router.post("/users", registerController.store);
 router.patch("/users/:id", registerController.update);
-router.delete("/users/:id", registerController.destroy); //no se puede borrar si todavía no borraste la orden
+router.delete("/users/:id", registerController.destroy); //no se puede borrar un susario si todavía no borraste las órdenes de dicho usuario
 
 // Mostrar productos en home y en página de producto
 router.get("/products", productController.showHome);
@@ -20,7 +20,7 @@ router.get("/products/:slug", productController.showSingleProduct);
 // CRUD de productos
 router.post("/products", productController.store);
 router.patch("/products/slug o id", checkAuthor, productController.update);
-router.delete("/product", checkAuthor, productController.destroy); //no se puede borrar si todavía no borraste la orden, ya no borrar, sino que dejar inactivo
+router.delete("/product", checkAuthor, productController.destroy); //no se puede borrar producto si todavía no borraste las órdenes que contengan dicho producto, ya no borrar, sino que dejar inactivo
 
 router.use(
   checkToken({ secret: process.env.SECRET_TEXT, algorithms: ["HS256"] })
