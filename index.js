fa9c76e4a4e84798e1047d2d6abb56e2fc03f652
Alejandro Schmidt <alemusho@gmail.com>
2021-05-20 09:32:51 -0300

    ok

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-//const adminRoutes = require("./routes/adminRoutes");
-app.use(cors());
 const routes = require("./routes/apiRoutes");
-
-// const seeder = require("./seeder");
+const seeder = require("./seeder");
 app.set("port", 3000);
 // const db = require("./models/index");
 
-// SOLO PARA CREAR TABLA DESDE CERO y borra tablas viejas
+// SOLO PARA CREAR TABLA DESDE CERO (borra tablas viejas y carga seeder)
+
 // db.sequelize.sync({ force: true }).then(() => {
 //   seeder();
 //   console.log("Tables created!");
@@ -18,13 +16,8 @@ app.set("port", 3000);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(adminRoutes);
-
 app.use(routes);
-
-// app.use(express.static(path.join(__dirname, "public")));
-// app.use(express.static(__dirname + "/public"));
-// app.set("view engine", "ejs");
+app.use(cors());
 
 app.listen(app.get("port"), () => {
   console.log(`Server running in http://localhost:${app.get("port")}`);
